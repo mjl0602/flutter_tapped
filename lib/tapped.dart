2fbac64cd893b9eb5395660fd2baf0d90cef7bd1
mjl0602 <675531696@qq.com>
2019-10-23 16:32:14 +0800

    init

---
+++ lib/tapped.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class Tapped extends StatefulWidget {
+  Tapped({this.child, this.onTap, this.onLongTap});
+  final Widget child;
+  final Function onTap;
+  final Function onLongTap;
+
+  @override
+  _TappedState createState() => _TappedState();
+}
+
+class _TappedState extends State<Tapped> with TickerProviderStateMixin {
+  bool _isChangeAlpha = false;
+
+  AnimationController _controller;
+  Animation _animation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(value: 1, vsync: this);
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOutCubic,
+    );
+    super.initState();
+  }
+
+  bool _tapDown = false;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Duration duration = const Duration(milliseconds: 50);
+    Duration showDuration = const Duration(milliseconds: 660);
+    _animation.addListener(() {
+      this.setState(() {});
+    });
+    return GestureDetector(
+      onTap: () async {
+        await Future.delayed(Duration(milliseconds: 100));
+        widget.onTap?.call();
+      },
+      onLongPress: () async {
+        await Future.delayed(Duration(milliseconds: 100));
+        widget.onLongTap?.call();
+      },
+      onTapDown: (detail) async {
+        _tapDown = true;
+        _isChangeAlpha = true;
+        await _controller.animateTo(0.4, duration: duration);
+        if (!_tapDown) {
+          await _controller.animateTo(1, duration: showDuration);
+        }
+        _tapDown = false;
+        _isChangeAlpha = false;
+      },
+      onTapUp: (detail) async {
+        _tapDown = false;
+        if (_isChangeAlpha == true) {
+          return;
+        }
+        await _controller.animateTo(1, duration: showDuration);
+        _isChangeAlpha = false;
+      },
+      onTapCancel: () async {
+        _tapDown = false;
+        _controller.value = 1;
+        _isChangeAlpha = false;
+      },
+      child: Opacity(
+        opacity: _animation.value,
+        child: widget.child,
+      ),
+    );
+  }
+}
